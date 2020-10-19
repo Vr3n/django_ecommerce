d6b983d713ceb743c421325069afa4aaedb6151e
vr3n <vickyspatel@gmail.com>
2020-10-19 17:38:52 +0530

    Created test for custom user model and created model layout for shops

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+
+# Customer
+    # User
+    # first name
+    # last name
+    # email
+    # phone
+    # creation_date
+    # update_date
+
+
+# Address
+    # Address
+    # Customer
+    # creation_date
+    # update_date
+
+
+
+# Category
+    # name
+
+# Distributor
+    # name
+    # address
+    # phone
+    # email
+    # creation_date
+    # update_date
+
+
+# Product
+    # name
+    # price
+    # Category
+    # Distributor
+    # creation_date
+    # tags
+    # update_date
+
+
+# Tag
+    # name
+
+
+# Order
+
+    # STATUS_CHOICES 
+
+    # Customer
+    # product
+    # Distributor
+    # creation_date
+    # update_date
+    # order_status
+
+
+
+# Cart
+    # Product
+    # Customer
